09f60a2a1138314de055752ff564f6aac51e4768
Seongbeom27 <lgh121546@naver.com>
2024-08-10 17:44:43 +0900

    docker container 내 rabbitmq 연결 성공

--- listener-service/main.go
+++ listener-service/main.go
@@ -1,11 +1,59 @@
 package main
 
-func main() {
-	// try to connect rabbitmq
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"time"
 
-	//  start listening for messages
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	// try to connect to rabbitmq
+	rabbitConn, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer rabbitConn.Close()
+
+	// start listening for messages
+	log.Println("Listening for and consuming RabbitMQ messages...")
 
 	// create consumer
 
 	// watch the queue and consume events
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backOff = 1 * time.Second
+	var connection *amqp.Connection
+
+	// don't continue until rabbit is ready
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@localhost:5672")
+		if err != nil {
+			fmt.Println("RabbitMQ not yet ready...")
+			counts++
+		} else {
+			log.Println("Connected to RabbitMQ!")
+			connection = c
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("backing off...")
+		time.Sleep(backOff)
+		continue
+	}
+
+	return connection, nil
 }
